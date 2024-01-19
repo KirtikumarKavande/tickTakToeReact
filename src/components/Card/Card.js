242992d6ad0a9499c72e675f890ff663504abbe9
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-01-19 19:14:02 +0530

    added playground tick toe

---
+++ src/components/Card/Card.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./card.css"
+import Grid from "../Grid/Grid";
+const Card = () => {
+  return <div className="card">
+    <Grid/>
+    <Grid/>
+    <Grid/>
+    <Grid/>
+    <Grid/>
+    <Grid/>
+    <Grid/>
+    <Grid/>
+    <Grid/>
+  </div>;
+};
+
+export default Card;
---
+++ src/components/Grid/Grid.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './grid.css'
+const Grid = () => {
+  return (
+    <div className='grid'>
+        *
+    </div>
+  )
+}
+
+export default Grid
